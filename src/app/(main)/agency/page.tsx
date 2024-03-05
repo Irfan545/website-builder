bf54b2b0fa64ecde0cd07d85880e3127ca470638
Irfan545 <controlaltdefeat2@gmail.com>
2024-03-05 07:06:45 +0530

    Added Agency and subAccounts registration and validation. And Added sideBar options to the agency

--- src/app/(main)/agency/page.tsx
+++ src/app/(main)/agency/page.tsx
@@ -1,3 +1,4 @@
+import AgencyDetails from "@/components/forms/agency-details";
 import { getAuthUserDetails, verifyAndAcceptInvitation } from "@/lib/queries";
 import { currentUser } from "@clerk/nextjs";
 import { Plan } from "@prisma/client";
@@ -19,10 +20,13 @@ const page = async ({
   // get users details
 
   const user = await getAuthUserDetails();
+
   if (agencyId) {
     if (user?.role === "SUBACCOUNT_GUEST" || user?.role === "SUBACCOUNT_USER") {
       return redirect("/subaccount");
     } else if (user?.role === "AGENCY_OWNER" || user?.role === "AGENCY_ADMIN") {
+      // redirecting user to agency page according to their plan.
+
       if (searchParams.plan) {
         return redirect(
           `/agency/${agencyId}/billing?plan=${searchParams.plan}`
@@ -30,12 +34,31 @@ const page = async ({
       }
       if (searchParams.state) {
         const statePath = searchParams.state.split("__")[0];
-        const stateAgencyId = searchParams.state.split("__")[0];
-      }
+        const stateAgencyId = searchParams.state.split("___")[1];
+
+        //If agency ID is missing in state.
+
+        if (!stateAgencyId) return <div>Not Authorized</div>;
+
+        return redirect(
+          `/agency/${stateAgencyId}/${statePath}?code=${searchParams.code}`
+        );
+      } else return redirect(`/agency/${agencyId}`);
+    } else {
+      return <div>Not Authorized</div>;
     }
   }
-
-  return <div>Agency Dashboard</div>;
+  const authUser = await currentUser();
+  return (
+    <div className="flex justify-center items-center mt-4">
+      <div className="max-w-[850px] border[1px] p-4 rounded-xl">
+        <h1 className=" text-4xl">Create An Agency</h1>
+        <AgencyDetails
+          data={{ companyEmail: authUser?.emailAddresses[0].emailAddress }}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default page;
